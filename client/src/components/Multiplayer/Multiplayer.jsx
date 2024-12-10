8e963c60ff7dc5d570d7ab24de354e6a3061c027
Max Stein <mhs8558@rit.edu>
2024-12-10 15:01:04 -0500

    sdfs

--- client/src/components/Multiplayer/Multiplayer.jsx
+++ client/src/components/Multiplayer/Multiplayer.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { Layout, Space, Button, message } from 'antd';
 import Dice from '../../pages/Dice';
-import Scoreboard from '../Scoreboard/ScoreBoard';
+import Scoreboard from '../Scoreboard/Scoreboard';
 import { handleRollDice, toggleDiceSelection } from '../../services/diceService';
 import { calculateScores } from '../../services/scoreTurnService';
 
