11de7e10e91c7c9b8301fd1d8c7b0e438087c696
Max Stein <mhs8558@rit.edu>
2024-12-10 09:58:57 -0500

    asf

--- client/src/pages/Lobby/Lobby.jsx
+++ client/src/pages/Lobby/Lobby.jsx
@@ -1,352 +1,73 @@
-import React from 'react';
-import { Layout, Typography, Button, Space, Spin, Dropdown, Menu } from 'antd';
-import { DownOutlined } from '@ant-design/icons';
-import MultiplayerModal from '../../components/Multiplayer/MultiplayerModal';
-import Scoreboard from '../../components/ScoreBoard/ScoreBoard';
-import Dice from '../Dice';
-import '../../styles/Lobby.css';
-import PropTypes from 'prop-types';
-
-const { Header, Content } = Layout;
-const { Title, Text } = Typography;
-
-const LoadingView = () => (
-  <Layout style={{ height: '100vh' }}>
-    <Header className="top-nav">
-      <Space style={{ width: '100%', justifyContent: 'center' }}>
-        <Spin size="large" />
-      </Space>
-    </Header>
-    <Content style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
-      <Text>Loading game...</Text>
-    </Content>
-  </Layout>
-);
-
-const GameHeader = ({ 
-  currentPlayer, 
-  handleNewGame, 
+const LobbyView = ({
+  currentPlayer,
+  gameId,
+  diceValues,
+  selectedDice,
+  isRolling,
+  rollCount,
+  playerCategories,
+  handleNewGame,
   handleLogout,
+  handleRollDice,
+  toggleDiceSelection,
+  shouldResetScores,
+  handleScoreCategoryClick,
+  calculateScores,
+  onTurnComplete,
+  isLoading,
+  opponentState,
+  // Add new props for multiplayer functionality
   socket,
   isMultiplayerModalVisible,
   setIsMultiplayerModalVisible,
   onPlayerSelect
 }) => {
-  const menu = (
-    <Menu>
-      <Menu.Item key="single" onClick={() => handleNewGame('singleplayer')}>
-        Single Player
-      </Menu.Item>
-      <Menu.Item key="multi" onClick={() => handleNewGame('multiplayer')}>
-        Multiplayer
-      </Menu.Item>
-    </Menu>
-  );
-
-  return (
-    <Header className="top-nav">
-      <Space style={{ width: '100%', justifyContent: 'space-between' }}>
-        <Dropdown menu={menu} trigger={['click']}>
-          <Button>
-            New Game <DownOutlined />
-          </Button>
-        </Dropdown>
-        <Space>
-          <span style={{ color: 'white' }}>
-            {currentPlayer?.name ? `Welcome, ${currentPlayer.name}` : 'Loading...'}
-          </span>
-          <Button onClick={handleLogout} type="primary" danger>
-            Logout
-          </Button>
-        </Space>
-      </Space>
-
-      <MultiplayerModal
-        visible={isMultiplayerModalVisible}
-        onClose={() => setIsMultiplayerModalVisible(false)}
-        onPlayerSelect={onPlayerSelect}
-        currentPlayerId={currentPlayer?.player_id}
-        socket={socket}
-      />
-    </Header>
-  );
-};
-
-<GameHeader 
-  currentPlayer={props.currentPlayer}
-  handleNewGame={props.handleNewGame}
-  handleLogout={props.handleLogout}
-  socket={props.socket}
-  isMultiplayerModalVisible={props.isMultiplayerModalVisible}
-  setIsMultiplayerModalVisible={props.setIsMultiplayerModalVisible}
-  onPlayerSelect={props.onPlayerSelect}
-/>
-
-const PlayerSection = ({ 
-  isOpponent,
-  name,
-  dice,
-  selectedDice,
-  isRolling,
-  rollCount,
-  onDiceClick,
-  handleRoll,
-  disabled,
-  isOpponentTurn,
-  lastMove
-}) => (
-  <div className="player-section">
-    <Title level={4}>{name}</Title>
-    <div className="game-dice-container">
-      {dice.map((value, index) => (
-        <Dice
-          key={index}
-          value={value}
-          isSelected={!isOpponent && selectedDice.includes(index)}
-          isRolling={isRolling}
-          onClick={() => onDiceClick(index)}
-          disabled={disabled || isOpponent}
-        />
-      ))}
-    </div>
-    <div className="roll-button-container">
-      {!isOpponent ? (
-        <Button
-          type="primary"
-          onClick={handleRoll}
-          disabled={disabled || isOpponentTurn}
-          style={{
-            cursor: (disabled || isOpponentTurn) ? 'not-allowed' : 'pointer',
-            opacity: (disabled || isOpponentTurn) ? 0.5 : 1
-          }}
-        >
-          Roll Dice
-        </Button>
-      ) : (
-        <div>
-          <Text className="opponent-status" style={{ 
-            color: isOpponentTurn ? '#1890ff' : '#666',
-            fontWeight: isOpponentTurn ? 'bold' : 'normal'
-          }}>
-            {isOpponentTurn ? "Opponent is rolling..." : "Waiting for player..."}
-          </Text>
-          {lastMove && (
-            <Text className="last-move" style={{ 
-              display: 'block',
-              fontSize: '12px',
-              color: '#666'
-            }}>
-              Last move: {lastMove}
-            </Text>
-          )}
-        </div>
-      )}
-      <Text className="roll-count" style={{ marginLeft: '8px' }}>
-        Roll Count: {Math.min(rollCount, 3)}/3
-      </Text>
-    </div>
-  </div>
-);
-
-PlayerSection.propTypes = {
-  isOpponent: PropTypes.bool,
-  name: PropTypes.string.isRequired,
-  dice: PropTypes.arrayOf(PropTypes.number).isRequired,
-  selectedDice: PropTypes.arrayOf(PropTypes.number).isRequired,
-  isRolling: PropTypes.bool.isRequired,
-  rollCount: PropTypes.number.isRequired,
-  onDiceClick: PropTypes.func.isRequired,
-  handleRoll: PropTypes.func,
-  disabled: PropTypes.bool.isRequired,
-  isOpponentTurn: PropTypes.bool.isRequired,
-  lastMove: PropTypes.string
-};
-
-const GameBoard = ({
-  currentPlayer,
-  diceValues,
-  selectedDice,
-  isRolling,
-  rollCount,
-  handleRollDice,
-  toggleDiceSelection,
-  opponentState
-}) => (
-  <div className="game-board">
-    <PlayerSection
-      name={currentPlayer?.name || 'Player'}
-      dice={diceValues}
-      selectedDice={selectedDice}
-      isRolling={isRolling}
-      rollCount={rollCount}
-      onDiceClick={toggleDiceSelection}
-      handleRoll={handleRollDice}
-      disabled={rollCount >= 3}
-      isOpponentTurn={opponentState.isOpponentTurn}
-    />
-
-    <PlayerSection
-      isOpponent
-      name="Opponent"
-      dice={opponentState.dice}
-      selectedDice={[]}
-      isRolling={opponentState.isOpponentTurn}
-      rollCount={opponentState.rollCount}
-      onDiceClick={() => {}}
-      disabled={true}
-      isOpponentTurn={opponentState.isOpponentTurn}
-      lastMove={opponentState.lastCategory ? 
-        `Scored ${opponentState.turnScore} in ${opponentState.lastCategory}` : 
-        undefined}
-    />
-  </div>
-);
-
-GameBoard.propTypes = {
-  currentPlayer: PropTypes.shape({
-    name: PropTypes.string,
-    player_id: PropTypes.string
-  }),
-  diceValues: PropTypes.arrayOf(PropTypes.number).isRequired,
-  selectedDice: PropTypes.arrayOf(PropTypes.number).isRequired,
-  isRolling: PropTypes.bool.isRequired,
-  rollCount: PropTypes.number.isRequired,
-  handleRollDice: PropTypes.func.isRequired,
-  toggleDiceSelection: PropTypes.func.isRequired,
-  opponentState: PropTypes.shape({
-    categories: PropTypes.array,
-    dice: PropTypes.arrayOf(PropTypes.number),
-    score: PropTypes.number,
-    rollCount: PropTypes.number,
-    isOpponentTurn: PropTypes.bool,
-    lastCategory: PropTypes.string,
-    turnScore: PropTypes.number
-  }).isRequired
-};
-
-const ScoreboardContainer = ({
-  gameId,
-  currentPlayer,
-  playerCategories,
-  calculateScores,
-  diceValues,
-  rollCount,
-  handleScoreCategoryClick,
-  onTurnComplete,
-  shouldResetScores,
-  opponentState
-}) => (
-  <div className="scoreboards-container" style={{ display: 'flex', justifyContent: 'space-around', padding: '20px' }}>
-    {playerCategories && playerCategories.length > 0 ? (
-      <Scoreboard
-        key={`player-${gameId}`}
-        gameId={gameId}
-        currentPlayer={currentPlayer}
-        playerCategories={playerCategories}
-        calculateScores={calculateScores}
-        diceValues={diceValues}
-        rollCount={rollCount}
-        handleScoreCategoryClick={handleScoreCategoryClick}
-        onTurnComplete={onTurnComplete}
-        shouldResetScores={shouldResetScores}
-        isOpponent={false}
-      />
-    ) : (
-      <div style={{ textAlign: 'center', padding: '20px' }}>
-        <Spin />
-        <Text>Initializing scoreboard...</Text>
-      </div>
-    )}
-
-    {opponentState.categories && opponentState.categories.length > 0 ? (
-      <Scoreboard
-        key={`opponent-${gameId}-${shouldResetScores}`}  // Changed this line
-        gameId={gameId}
-        currentPlayer={{ name: 'Opponent', player_id: '9' }}
-        playerCategories={opponentState.categories}
-        calculateScores={calculateScores}
-        diceValues={opponentState.dice}
-        rollCount={opponentState.rollCount}
-        handleScoreCategoryClick={() => {}}
-        onTurnComplete={() => {}}
-        shouldResetScores={shouldResetScores}
-        isOpponent={true}
-      />
-    ) : (
-      <div style={{ textAlign: 'center', padding: '20px' }}>
-        <Spin />
-        <Text>Initializing opponent scoreboard...</Text>
-      </div>
-    )}
-  </div>
-);
-
-ScoreboardContainer.propTypes = {
-  gameId: PropTypes.string,
-  currentPlayer: PropTypes.shape({
-    name: PropTypes.string,
-    player_id: PropTypes.string
-  }),
-  playerCategories: PropTypes.array.isRequired,
-  calculateScores: PropTypes.func.isRequired,
-  diceValues: PropTypes.arrayOf(PropTypes.number).isRequired,
-  rollCount: PropTypes.number.isRequired,
-  handleScoreCategoryClick: PropTypes.func.isRequired,
-  onTurnComplete: PropTypes.func.isRequired,
-  shouldResetScores: PropTypes.bool.isRequired,
-  opponentState: PropTypes.shape({
-    categories: PropTypes.array,
-    dice: PropTypes.arrayOf(PropTypes.number),
-    score: PropTypes.number,
-    rollCount: PropTypes.number,
-    isOpponentTurn: PropTypes.bool,
-    lastCategory: PropTypes.string,
-    turnScore: PropTypes.number
-  }).isRequired
-};
-
-const LobbyView = (props) => {
-  if (props.isLoading) {
+  if (isLoading) {
     return <LoadingView />;
   }
 
   return (
     <Layout style={{ height: '100vh' }}>
       <GameHeader 
-        currentPlayer={props.currentPlayer}
-        handleNewGame={props.handleNewGame}
-        handleLogout={props.handleLogout}
+        currentPlayer={currentPlayer}
+        handleNewGame={handleNewGame}
+        handleLogout={handleLogout}
+        socket={socket}
+        isMultiplayerModalVisible={isMultiplayerModalVisible}
+        setIsMultiplayerModalVisible={setIsMultiplayerModalVisible}
+        onPlayerSelect={onPlayerSelect}
       />
 
       <Content className="game-container">
         <GameBoard
-          currentPlayer={props.currentPlayer}
-          diceValues={props.diceValues}
-          selectedDice={props.selectedDice}
-          isRolling={props.isRolling}
-          rollCount={props.rollCount}
-          handleRollDice={props.handleRollDice}
-          toggleDiceSelection={props.toggleDiceSelection}
-          opponentState={props.opponentState}
+          currentPlayer={currentPlayer}
+          diceValues={diceValues}
+          selectedDice={selectedDice}
+          isRolling={isRolling}
+          rollCount={rollCount}
+          handleRollDice={handleRollDice}
+          toggleDiceSelection={toggleDiceSelection}
+          opponentState={opponentState}
         />
 
         <ScoreboardContainer
-          gameId={props.gameId}
-          currentPlayer={props.currentPlayer}
-          playerCategories={props.playerCategories}
-          calculateScores={props.calculateScores}
-          diceValues={props.diceValues}
-          rollCount={props.rollCount}
-          handleScoreCategoryClick={props.handleScoreCategoryClick}
-          onTurnComplete={props.onTurnComplete}
-          shouldResetScores={props.shouldResetScores}
-          opponentState={props.opponentState}
+          gameId={gameId}
+          currentPlayer={currentPlayer}
+          playerCategories={playerCategories}
+          calculateScores={calculateScores}
+          diceValues={diceValues}
+          rollCount={rollCount}
+          handleScoreCategoryClick={handleScoreCategoryClick}
+          onTurnComplete={onTurnComplete}
+          shouldResetScores={shouldResetScores}
+          opponentState={opponentState}
         />
       </Content>
     </Layout>
   );
 };
 
+// Update LobbyView PropTypes to include new props
 LobbyView.propTypes = {
   currentPlayer: PropTypes.shape({
     name: PropTypes.string,
@@ -376,6 +97,9 @@ LobbyView.propTypes = {
     lastCategory: PropTypes.string,
     turnScore: PropTypes.number
   }).isRequired,
+  // Add new prop types
+  socket: PropTypes.object,
+  isMultiplayerModalVisible: PropTypes.bool,
+  setIsMultiplayerModalVisible: PropTypes.func,
+  onPlayerSelect: PropTypes.func
 };
-
-export default LobbyView;
