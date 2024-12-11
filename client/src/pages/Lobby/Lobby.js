c9ece5a28e05a116ba74eea0e24262fd25220dc9
Max Stein <mhs8558@rit.edu>
2024-12-11 14:41:21 -0500

    afs

--- client/src/pages/Lobby/Lobby.js
+++ client/src/pages/Lobby/Lobby.js
@@ -17,7 +17,7 @@ function Lobby() {
         const playerInfo = await fetchCurrentPlayer(navigate);
         if (playerInfo) {
           setCurrentPlayer(playerInfo.playerData);
-          console.log('Player info:', playerInfo);
+          console.log('Player info:', playerInfo.playerData.name);
 
           const socket = initializeWebSocket(playerInfo.playerData.player_id);
 
